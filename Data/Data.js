9132ce661b9d01fba5e1b73b85f537df9f20f2e0
hang long <sapbatcheung@gmail.com>
2021-07-06 17:39:37 +0700

    new

---
+++ Data/Data.js
@@ -0,0 +1,6 @@
+const songs = [
+  {Title: 'Colossus', artist: 'Shadow', image: require('../img/Colossus.jpg'), id: "1"},
+  {Title: 'FCK', artist: 'FCK', image: require('../img/FCK.jpg'), id: "2"},
+  {Title: 'God of War', artist: 'God', image: require('../img/gow.png'), id: "3"},
+  {Title: 'Legends', artist: 'Ghost of Tsushima', image: require('../img/Legends.jpg')}
+];
