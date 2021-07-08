ff1bc8ee3c67489498e0388881f655e5bcdf8449
hang long <sapbatcheung@gmail.com>
2021-07-08 11:21:19 +0700

    pictures

--- Data/Data.js
+++ Data/Data.js
@@ -1,8 +1,8 @@
-const datas = [
-  {Title: 'Colossus', artist: 'Shadow', image: require('../img/Colossus.jpg'), id: "1"},
-  {Title: 'FCK', artist: 'FCK', image: require('../img/FCK.jpg'), id: "2"},
-  {Title: 'God of War', artist: 'God', image: require('../img/gow.jpg'), id: "3"},
-  {Title: 'Legends', artist: 'Ghost of Tsushima', image: require('../img/Legends.png'), id: "4"}
+const songs = [
+  {title: "Colossus", artist: "Shadow", image: require("../img/Colossus.jpg"), id: "1"},
+  {title: "FCK", artist: "FCK", image: require("../img/FCK.jpg"), id: "2"},
+  {title: "God", artist: "God", image: require("../img/gow.jpg"), id: "3"},
+  {title: "Legends", artist: "Ghost", image: require("../img/Legends.png"), id: "4"}
 ];
 
-export default datas; 
+export default songs; 
--- screens/SongPlayer.js
+++ screens/SongPlayer.js
@@ -19,6 +19,7 @@ import {
   ImageView,
   Dimensions,
   TouchableOpacity,
+  FlatList,
 } from 'react-native';
 
 import {Icon} from 'react-native-elements';
@@ -28,77 +29,179 @@ import {
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 
+import songs from '../Data/Data';
+
 import Slider from '@react-native-community/slider';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const {width, height} = Dimensions.get('window');
 
-class SongPlayer extends Component {
-  render() {
+const SongPlayer = () => {
+
+  const renderSongs = ({index, item}) => {
+    
+      return (
+          <View style={styles.img}> 
+            <Image
+              source={require('../img/Colossus.jpg')}
+              style={styles.artwork}></Image>
+          </View>
+      );
+  }
+
+  
     return (
       <SafeAreaView style={styles.container}>
-        <View style={styles.mainContainer}></View>
-        <View
-          style={styles.bottomContainer}>
-          <View
-            style={styles.bottomControls}>
+        <View style={styles.mainContainer}>
+          <FlatList
+          data={songs}
+          renderItems={renderSongs}
+          keyExtractor={(item) => item.id}
+          horizontal
+          pagingEnabled
+          showHorizontalScrollIndicator={false}
+          scrollEventThrottle={16}
+           />
+          <View>
+            <Text style={styles.txt}>Song Title</Text>
+            <Text style={styles.txt}>Song Artist Name</Text>
+          </View>
+          <View>
+            <Slider
+              style={styles.slider}
+              minimumValue={0}
+              maximumValue={100}
+              minimumTrackTintColor="#da70d6"
+              maximumTrackTintColor="#000000"
+              thumbTintColor="#FFD369"
+              minimumTrackTintColor="#FFD369"
+              maximumTrackTintColor="#FFD369"
+              onSlidingComplete={() => {}}              
+            />
+            <View style={styles.progressLabelContainer}>
+              <Text style={styles.ProgressLabelTxt}>0:00</Text>
+              <Text style={styles.ProgressLabelTxt}>3.55</Text>
+            </View>
+          </View>
+          <View style={styles.musicControls}>
             <TouchableOpacity onPress={() => {}}>
-            <Ionicons name="heart-outline" size={30} />
-            </TouchableOpacity> 
-            <TouchableOpacity onPress={() => {}}>
-            <Ionicons name="repeat" size={30} />
+              <Ionicons name="play-skip-back-outline" size={30} color="#ffff00" style={{marginTop: 15}}/>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => {}}>
-            <Ionicons name="share-outline" size={30} />
+              <Ionicons name="ios-pause-circle-outline" size={60} color="#ffff00" />
             </TouchableOpacity>
             <TouchableOpacity onPress={() => {}}>
-            <Ionicons name="ellipsis-horizontal" size={30} />
+              <Ionicons name="play-skip-forward-outline" size={30} color="#ffff00" style={{marginTop: 15}} />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.bottomContainer}>
+          <View style={styles.bottomControls}>
+            <TouchableOpacity onPress={() => {}}>
+              <Ionicons name="heart-outline" size={30} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {}}>
+              <Ionicons name="repeat" size={30} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {}}>
+              <Ionicons name="share-outline" size={30} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {}}>
+              <Ionicons name="ellipsis-horizontal" size={30} />
             </TouchableOpacity>
           </View>
         </View>
       </SafeAreaView>
     );
-  }
+  
 }
 export default SongPlayer;
 
 const styles = StyleSheet.create({
-  // Beginning of Container 1
+  // Beginning of Container
   container: {
     flex: 1,
-    backgroundColor: '#a9a9a9',
+    backgroundColor: 'rgb(56,56,56)',
   },
   mainContainer: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
-  // End of Container 1
+  // End of Container
 
-  // Beginning of Container 2
-
-  // End of Container 2
-
-  // Beginning of Container 3
-
-  // End of Container 3
-
-  // Beginning of Container 4
-
-  // End of Container 4
-
-  // Beginning of Container 5
-  bottomContainer:
-  {
-    borderTopWidth: 1, 
-    borderTopColor: 'black', 
+  // Beginning of image
+  img: {
+    width: 300,
+    height: 350,
+    marginBottom: 25,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  artwork: {
     width: '100%',
-    
+    height: '100%',
+    resizeMode: 'contain',
+    borderRadius: 30,
+  },
+
+  // End of Image
+
+  // Beginning of text
+  txt: {
+    color: '#fffaf0',
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  // End of text
+
+  // Beginning of Slider
+
+  slider: {
+    width: 350,
+    height: 40,
+    marginTop: 20,
+    flexDirection: 'row',
+  },
+  progressLabelContainer: {
+    width: 340,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  progressLabelTxt: {
+    color: '#228b22',
+  },
+  // End of Slider
+
+// Beginning of music Controls
+  musicControls: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '60%',
+    marginTop: 20,
+  },
+
+// End of music Controls
+
+  // Beginning of bottom Container
+  bottomContainer: {
+    borderTopWidth: 1,
+    borderTopColor: 'black',
+    width: '100%',
+    alignItems: 'center',
+    paddingVertical: 10,
   },
   bottomControls: {
     flexDirection: 'row',
     justifyContent: 'space-around',
     width: '100%',
-  }
+  },
   // End of Container 5
 });
